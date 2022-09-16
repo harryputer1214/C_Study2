37c89da0f0ac543842215fef03430d42415f04c3
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-09-16 17:22:47 +0900

    Create w1_LightItUp1.c

---
+++ w1_LightItUp1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  double height[4];
+  int j=2;
+
+  height[0]=145.7;
+  height[1]=156.3;
+  height[j]=147.89;
+  j++;
+  height[j]=159.4;
+  return 0;
+}
+
+//w1_LightItUp1 다음 프로그램을 실행해 봅시다.
