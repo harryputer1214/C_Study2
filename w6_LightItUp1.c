057d917f0eee0bc8489910574075147496cf67dd
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-10-21 16:44:35 +0900

    Update w6_LightItUp1.c

--- w6_LightItUp1.c
+++ w6_LightItUp1.c
@@ -10,12 +10,18 @@ int main() {
   struct COORDI p2 = {5, 9};
   struct COORDI p3;
   struct COORDI p4;
-  
+
   p1.x = 2;
   p1.y = 3;
 
-  printf("p1의 좌표(%d, %d)\n",p1.x,p1.y);
-  printf("p2의 좌표(%d, %d)\n",p2.x,p2.y);
+  printf("p1의 좌표(%d, %d)\n", p1.x, p1.y);
+  printf("p2의 좌표(%d, %d)\n", p2.x, p2.y);
+
+  scanf("%d %d", &p3.x, &p3.y);
+  p4 = p2;
+  
+  printf("p3의 좌표(%d, %d)\n", p3.x, p3.y);
+  printf("p4의 좌표(%d, %d)\n", p4.x, p4.y);
 
   return 0;
 }
